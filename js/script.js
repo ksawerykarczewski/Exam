5b86e0a6e55dcf24ef9445c591f9ef9f2055d0af
Ksawery Karczewski <koscisty@MacBook-Pro-Ksawery.local>
2019-05-29 14:30:58 +0200

    TEST ON MOBILE LANDING PAGE + MODAL

--- js/script.js
+++ js/script.js
@@ -19,3 +19,16 @@ function show(projects) {
     });
 }
 loadAll()
+
+
+
+// -------------------------------------------  GET MODAL
+
+
+function openNav() {
+  document.getElementById("myNav").style.display = "block";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.display = "none";
+}
