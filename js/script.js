1c85702a1b43856d3fca866cfe97f2ef5df40f7f
Ksawery Karczewski <koscisty@MacBook-Pro-Ksawery.local>
2019-05-30 11:44:20 +0200

    bio box + footer + commented out fetch for bios

--- js/script.js
+++ js/script.js
@@ -4,6 +4,25 @@ const template = document.querySelector("template").content;
 const parent = document.querySelector("main");
 const baseLink = "http://sansindesign.com/wp_kajetan/wp-json/wp/v2/"
 
+
+/* -------------------- BIOS */
+
+//function loadAll2() {
+//    fetch(baseLink + "bio").then(e => e.json()).then(show2);
+//}
+//
+//function show2(bios) {
+//    bios.forEach(bio => {
+//        const clone2 = document.getElementById("template_bios").cloneNode(true);
+//        clone2.querySelector(".short-bio").textContent = bio.short_bio;
+//        parent.appendChild(clone2);
+//    });
+//}
+//
+//loadAll2();
+
+/* -------------------- PROJECTS */
+
 function loadAll() {
     fetch(baseLink + "project?_embed").then(e => e.json()).then(show);
 }
@@ -14,11 +33,15 @@ function show(projects) {
         clone.querySelector(".year").textContent = project.project_year;
         clone.querySelector(".title").textContent = project.project_name;
         clone.querySelector("img").src = project._embedded["wp:featuredmedia"][0].media_details.sizes.large.source_url;
-        clone.querySelector(".technique").textContent = project.materialtechnique;
+//        clone.querySelector(".technique").textContent = project.materialtechnique;
         parent.appendChild(clone);
     });
+
 }
 loadAll()
+
+
+
 
 
 
@@ -26,9 +49,9 @@ loadAll()
 
 
 function openNav() {
-  document.getElementById("myNav").style.display = "block";
+    document.getElementById("myNav").style.display = "block";
 }
 
 function closeNav() {
-  document.getElementById("myNav").style.display = "none";
+    document.getElementById("myNav").style.display = "none";
 }
