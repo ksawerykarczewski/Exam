eeda32819e119169f2d3311fdf7d6a000183526f
Ksawery Karczewski <koscisty@msi.kea.local>
2019-05-31 17:35:39 +0200

    footer with form + details js

--- js/details-script.js
+++ js/details-script.js
@@ -6,22 +6,57 @@ const urlParams = new URLSearchParams(window.location.search);
 
 const projectID = urlParams.get("id");
 
+const template = document.querySelector("template").content;
+
+const parent = document.querySelector("main");
+
+//const parent = document.querySelector("main");
+
 console.log(projectID);
 
 fetch(baseLink + projectID + "?_embed").then(e => e.json()).then(showProject);
 
 function showProject(data) {
-    document.querySelector(".year").textContent = data.project_year;
-    document.querySelector(".title").textContent = data.project_name;
-    document.querySelector(".long-description").textContent = data.description;
+
+    // clone the template
+
+    const clone = template.cloneNode(true);
+
+    // populate it
+
+    const year = clone.querySelector(".year");
+
+
+    year.textContent = data.project_year;
 
     for (let i = 0; i < data.image.length; i++) {
-
-        document.querySelector("img").src = data.image[i].guid;
-
+        let image = document.createElement('img');
+        image.classList.add('project-image');
+        image.src = data.image[i].guid;
+        clone.appendChild(image);
     }
 
+    // append to DOM
+
+    parent.appendChild(clone);
 }
+
+
+
+//    document.querySelector(".title").textContent = data.project_name;
+//    document.querySelector(".long-description").textContent = data.description;
+
+//    for (let i = 0; i < data.image.length; i++) {
+//        document.querySelector("img").src = data.image[i].guid;
+//    }
+
+    //    for (let i = 0; i < data.image.length; i++) {
+    //        let image = document.createElement('img');
+    //        image.classList.add('project-image');
+    //        image.src = data.image[i].guid;
+    //        document.appendChild(image);
+    //    }
+
 
 /* -------------------- GALLERY */
 //
