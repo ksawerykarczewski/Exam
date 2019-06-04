594346256a28597a7ed0471448edc8313ce79992
Martin <martingatski@gmail.com>
2019-06-04 10:31:20 +0200

    biopage improvement

--- js/bio-script.js
+++ js/bio-script.js
@@ -18,7 +18,7 @@ function show(projects) {
         clone.querySelector(".long-bio-heading").textContent = bio.long_bio_heading;
         clone.querySelector(".long-bio").textContent = bio.long_bio;
         clone.querySelector(".long-bio-2").textContent = bio.long_bio_2;
-        clone.querySelector("img").src = bio._embedded["wp:featuredmedia"][0].media_details.sizes.large.source_url;
+//        clone.querySelector("img").src = bio._embedded["wp:featuredmedia"][0].media_details.sizes.large.source_url;
         parent.appendChild(clone);
     });
 
